9e896956ff72e25211e2f3e563d29ac30d6e5356
HimeurWail <kw_himeur@esi.dz>
2024-05-12 19:48:21 +0100

    edit profile

--- routes/utilisateur.route.js
+++ routes/utilisateur.route.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const {get_users , signUpUser, signInUser, updateUsername} = require("../models/utilisateur.model");
+const {get_users , signUpUser, signInUser, updateUsername, updateEmail, updatePassword} = require("../models/utilisateur.model");
 const utilisateurRouter = express.Router();
 
 utilisateurRouter.route('/test').get(get_users)
 utilisateurRouter.route('/signup').post(signUpUser)
 utilisateurRouter.route('/signin').post(signInUser)
 utilisateurRouter.route('/update_username').post(updateUsername)
+utilisateurRouter.route('/update_email').post(updateEmail)
+utilisateurRouter.route('/update_password').post(updatePassword)
 
 module.exports = utilisateurRouter ;
