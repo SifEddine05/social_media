8be227c7eb8ce475a665b46e11c2477f371dc815
HimeurWail <kw_himeur@esi.dz>
2024-05-12 17:59:13 +0100

    signin modified

--- routes/utilisateur.route.js
+++ routes/utilisateur.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const {get_users , signUpUser, signInUser} = require("../models/utilisateur.model");
+const {get_users , signUpUser, signInUser, updateUsername} = require("../models/utilisateur.model");
 const utilisateurRouter = express.Router();
 
 utilisateurRouter.route('/test').get(get_users)
 utilisateurRouter.route('/signup').post(signUpUser)
 utilisateurRouter.route('/signin').post(signInUser)
+utilisateurRouter.route('/update_username').post(updateUsername)
 
 module.exports = utilisateurRouter ;
